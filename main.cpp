09052d056feb6d86a49fcd8fea038070449c13a8
ansonfoong <afoong@albany.edu>
2019-06-03 00:43:31 -0400

    ofstream -> ifstream

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main() {
 
-    ofstream asm_file;
+    ifstream asm_file;
     asm_file.open("sample.asm");
 
     if(asm_file.is_open())
---
+++ sample.asm
@@ -0,0 +1,6 @@
+li $t1, 1
+li $t2, 2
+add $t3, $t1, $t2
+
+addi $sp, $sp, -4
+sw $t3, 0($sp)
