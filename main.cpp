7f96be18f1a52c6a38452b4ebaa56b90e82a2061
ansonfoong <afoong@albany.edu>
2019-06-03 01:10:33 -0400

    Yeet

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <bits/stdc++.h>
+#include <stdlib.h>
 
 using namespace std;
 
@@ -7,10 +10,29 @@ int main() {
 
     ifstream asm_file;
     asm_file.open("sample.asm");
+    string line;
 
     if(asm_file.is_open())
     {
         cout << "Sucess\n";
+        // while(std::getline(asm_file, line))
+        // {
+        //     istringstream stream(line);
+        //     string token;
+        //     // while(getline(stream, token, ' '))
+        //     // {
+                
+                
+        //     // }
+        // }
+
+        stringstream stream;
+        stream << asm_file.rdbuf();
+        string str = stream.str();
+        if(str.find(".text"))
+        {
+            cout << "Found" << endl;
+        }
     }
     return 0;
 }
--- sample.asm
+++ sample.asm
@@ -1,6 +1,13 @@
+.data
+
+name: .asciiz "Anson"
+age: .word 20
+
+.text
 li $t1, 1
 li $t2, 2
 add $t3, $t1, $t2
 
 addi $sp, $sp, -4
 sw $t3, 0($sp)
+
